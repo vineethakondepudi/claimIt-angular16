7efb0f65d05b57332d34d699d3bb7d6c2e82e631
gmugada-at-646632670602 <Ganesh.Mugada@americold.com>
2024-12-09 18:57:15 +0530

    changes

--- src/app/@amc/components/contact/contact.component.ts
+++ src/app/@amc/components/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -22,6 +22,39 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   styleUrls: ['./contact.component.scss']
 })
 export default class ContactComponent {
-  email:any='support@lostandfoundapp.com'
+  supportEmail: any = 'support@lostandfoundapp.com'
+  contactForm!: FormGroup;
+  enableSave: boolean = true
+  emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  constructor(private fb: FormBuilder) {
 
+  }
+  ngOnInit() {
+    this.initializeContactForm()
+    this.contactForm.statusChanges.subscribe((res: any) => {
+      console.log('res', res)
+      if (res === 'VALID') {
+        this.enableSave = false
+      } else {
+        this.enableSave = true
+
+      }
+    })
+  }
+  initializeContactForm() {
+    this.contactForm = this.fb.group({
+      name: this.fb.control("", Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.emailRegex),
+      ]),
+       message: this.fb.control("")
+    })
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+  contactUs() {
+    console.log(this.contactForm)
+  }
 }
