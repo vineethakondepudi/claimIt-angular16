73d141aa8137a37e803bbfafed2053d754c11964
gmugada-at-646632670602 <Ganesh.Mugada@americold.com>
2024-12-09 19:40:53 +0530

    changes

--- src/app/@amc/components/about/about.component.ts
+++ src/app/@amc/components/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { FormFooterComponent } from '../form-footer/form-footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule,    MatIconModule,
+  imports: [CommonModule,FormFooterComponent ,   MatIconModule,
   ],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
