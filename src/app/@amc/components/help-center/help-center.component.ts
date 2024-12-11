dc52db9570216de33c197c225049995b6c819367
vineetha kondepudi <vineethakondepudi22@gmail.com>
2024-12-11 20:26:07 +0530

    Impletemented the help component

---
+++ src/app/@amc/components/help-center/help-center.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormFooterComponent } from '../form-footer/form-footer.component';
+
+@Component({
+  selector: 'app-help-center',
+  standalone: true,
+  imports: [CommonModule, FormFooterComponent, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  templateUrl: './help-center.component.html',
+  styleUrls: ['./help-center.component.scss']
+})
+export default class HelpCenterComponent {
+  supportEmail: string = 'support@helpapp.com';
+  helpForm!: FormGroup;
+  enableSave: boolean = true;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initializeHelpForm();
+    this.helpForm.statusChanges.subscribe((res: any) => {
+      this.enableSave = res !== 'VALID';
+    });
+  }
+
+  initializeHelpForm() {
+    this.helpForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      request: ['', Validators.required]
+    });
+  }
+
+  get email() {
+    return this.helpForm.get('email');
+  }
+
+  submitRequest() {
+    console.log('Request submitted:', this.helpForm.value);
+    // Handle form submission logic here, e.g., sending the data to the server
+    this.helpForm.reset();
+  }
+}
