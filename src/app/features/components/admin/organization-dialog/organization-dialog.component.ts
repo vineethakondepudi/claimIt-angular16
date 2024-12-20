b9431800a3f1f7deef469d182fbe42f70e0a0ea9
bdulipudi <bdulipudi@miraclesoft.com>
2024-12-20 19:45:14 +0530

    changes

--- src/app/features/components/admin/organization-dialog/organization-dialog.component.ts
+++ src/app/features/components/admin/organization-dialog/organization-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -9,6 +9,7 @@ import { ClaimitService } from 'src/app/features/sharedServices/claimit.service'
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from "@angular/material/divider";
+import { FormSubmissionModalComponent } from 'src/app/@amc/components/form-submission-modal/form-submission-modal.component';
 
 @Component({
   selector: 'app-organization-dialog',
@@ -20,35 +21,67 @@ import { MatDividerModule } from "@angular/material/divider";
 export class OrganizationDialogComponent {
   organizationList: any[] = [];
   isOrganizationSelected: boolean = false;
-  files: any[] = [];
+  files: { file: File, preview: string }[] = [];
+  
   selectedOrgId: string = '';
   selected: any;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<OrganizationDialogComponent>,
-    private service: ClaimitService
+    private service: ClaimitService,
+    private matDialog: MatDialog,
   ) {
     this.organizationList = data.organizationList;
+    console.log(this.organizationList);
+    
     if (this.organizationList.length > 0) {
-      this.selected = this.organizationList[0].orgId;
+      this.selected = this.organizationList[0].orgId;      
       this.selectedOrgId = this.selected; 
+      console.log(this.selectedOrgId);   
     }
+    this.onUploadImage();
+    
   }
 
   onOrganizationSelect(orgId: string): void {
     this.selectedOrgId = orgId;
   }
-
   onSelectFile(event: any): void {
-    this.files = [];
+    const allowedTypes = ['image/jpeg','image/png', 'image/gif', 'image/bmp', 'image/jfif'];
+    this.files = [];   
+    let validFiles = true;
     for (const file of event.addedFiles) {
-      this.files.push({
-        file: file,
-        preview: URL.createObjectURL(file),
-      });
+      if (allowedTypes.includes(file.type)) {
+        this.files.push({
+          file: file,
+          preview: URL.createObjectURL(file),  
+        });
+      } else {
+        validFiles = false;
+        const dialogRef = this.matDialog.open(FormSubmissionModalComponent, {
+          width: "500px",
+          data: {
+            status: 'Error',
+            msg: 'Only JPG, JPEG, PNG, GIF, BMP, and JFIF image formats are allowed.',
+            btnName: "OK",
+          },
+        });
+  
+        dialogRef.afterClosed().subscribe(() => {
+        });
+        console.error('Invalid file type:', file.type);
+        break;
+      }
+    }
+  
+    // If no valid files were found, reset files
+    if (!validFiles) {
+      this.files = [];
     }
   }
+  
+  
 
   onRemove(file: any): void {
     this.files = this.files.filter(f => f !== file);
@@ -58,6 +91,8 @@ export class OrganizationDialogComponent {
     if (this.files.length > 0 && this.selectedOrgId) {
       const formData = new FormData();
       formData.append('image', this.files[0].file);
+      console.log(this.files);
+      
       formData.append('orgId', this.selectedOrgId);
   
       this.service.adminUploadItem(this.selectedOrgId, formData).subscribe(
@@ -65,8 +100,8 @@ export class OrganizationDialogComponent {
           console.log('File uploaded successfully:', response);
           this.isOrganizationSelected = false;
           this.files = [];
+          console.log(this.files);
           
-         
           this.dialogRef.close();  
         },
         (error) => {
@@ -75,11 +110,8 @@ export class OrganizationDialogComponent {
       );
     } else {
       console.warn('No file selected for upload.');
-    }
-    
+    }    
   }
-
-
   submit(): void {
     if (this.files.length > 0 && this.selectedOrgId) {
       const formData = new FormData();
