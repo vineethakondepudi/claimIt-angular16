c1e8688d330ccd2eaa2a9bdb99cb035f267c396a
vineetha kondepudi <vineethakondepudi22@gmail.com>
2025-01-06 16:22:06 +0530

    updated

--- src/app/features/components/claim-request/claim-request.component.ts
+++ src/app/features/components/claim-request/claim-request.component.ts
@@ -6,16 +6,20 @@ import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-claim-request',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatIconModule, MatDatepickerModule, MatFormFieldModule],
+  imports: [CommonModule, FormsModule, MatIconModule, MatDatepickerModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './claim-request.component.html',
   styleUrls: ['./claim-request.component.scss']
 })
 export default class ClaimRequestComponent {
-  
+  selectedLocation!: string;
+  latitude: number | null = null;
+  longitude: number | null = null;
+  selectedFile: File | null = null; 
   claimData = {
     name: '',
     contact: '',
@@ -25,6 +29,7 @@ export default class ClaimRequestComponent {
     identification: '',
     additionalInfo: '',
     incidentDate: '', 
+    imageUrl:''
   };
 
   constructor(private claimService: ClaimitService, private router: Router ) {}
@@ -38,7 +43,47 @@ export default class ClaimRequestComponent {
     
     this.router.navigate(['/claimit/dashboard']);
   }
+  filterDates = (date: Date | null): boolean => {
+    const today = new Date();
+    return date ? date <= today : false;
+  };
 
+
+  onLocationChange(): void {
+    switch (this.selectedLocation) {
+      case 'Miracle City':
+        this.latitude = 17.739707548305788;
+        this.longitude = 83.3433213167522;
+        break;
+      case 'Miracle Valley': 
+        this.latitude = 17.814901263799708;
+        this.longitude =  83.39172982091242;
+        break;
+      case 'Miracle Heights': 
+        this.latitude = 17.809998290809897;
+        this.longitude =  83.39687966178053; 
+        break;
+      case 'Miracle Global HQ':
+        this.latitude = 42.48540914670753;
+        this.longitude = -83.49780041745922;
+        break;
+      default:
+        this.latitude = null;
+        this.longitude = null;
+    }
+  }
+  onFileSelect(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files.length > 0) {
+      this.selectedFile = fileInput.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.claimData.imageUrl = reader.result as string;
+      };
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+  
   resetForm() {
     this.claimData = {
       name: '',
@@ -48,7 +93,8 @@ export default class ClaimRequestComponent {
       location: '',
       identification: '',
       additionalInfo: '',
-      incidentDate: '',     
+      incidentDate: '',    
+      imageUrl:'' 
     };
   }
 }
