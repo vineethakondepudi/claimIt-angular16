ceb9c03cd64c3fc8a2f37bf291606343000f355c
KrishnaVedantam <Kvedantam@miraclesoft.com>
2025-01-20 19:39:52 +0530

    changing api

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    adminLogin:'http://172.17.12.38:8081/api/admin/login',
+    adminLogin:'http://172.17.12.101:8081/api/admin/login',
     getAllItems:'http://172.17.12.38:8081/api/users/claim-history',
     unClaim:'http://172.17.12.38:8081/api/users/updateStatus',
     listOfItems:'http://172.17.12.38:8081/api/admin/listOfItems',
